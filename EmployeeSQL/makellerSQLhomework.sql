14e6220f3c2af94d3b6a457d8246da63187bde05
Melissa Keller <kelle595@umn.edu>
2020-02-09 17:50:58 -0600

    added bonus, ERD, and updates

--- EmployeeSQL/makellerSQLhomework.sql
+++ EmployeeSQL/makellerSQLhomework.sql
@@ -114,4 +114,5 @@ where dept_name = 'Sales' or dept_name = 'Development';
 --8
 select last_name, count(last_name) as "Last Name Count"
 from employees
-group by last_name;
+group by last_name
+order by last_name desc;
