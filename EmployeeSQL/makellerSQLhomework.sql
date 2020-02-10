e86829875e1065d9b6e7ee96ac32667d007ff613
Melissa Keller <kelle595@umn.edu>
2020-02-09 18:41:00 -0600

    Possibly the final updates to the homework.

--- EmployeeSQL/makellerSQLhomework.sql
+++ EmployeeSQL/makellerSQLhomework.sql
@@ -1,75 +1,3 @@
--- -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- -- Link to schema: https://app.quickdatabasediagrams.com/#/d/lr7380
--- -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
--- CREATE TABLE "departments" (
---     "dept_no" varchar(30)   NOT NULL,
---     "dept_name" varchar(30)   NOT NULL,
---     CONSTRAINT "pk_departments" PRIMARY KEY (
---         "dept_no"
---      )
--- );
-
--- CREATE TABLE "dept_emp" (
---     "emp_no" integer   NOT NULL,
---     "dept_no" varchar(30)   NOT NULL,
---     "from_date" varchar(30)   NOT NULL,
---     "to_date" varchar(30)   NOT NULL
--- );
-
--- CREATE TABLE "dept_manager" (
---     "dept_no" varchar(30)   NOT NULL,
---     "emp_no" integer   NOT NULL,
---     "from_date" varchar(30)   NOT NULL,
---     "to_date" varchar(30)   NOT NULL
--- );
-
--- CREATE TABLE "employees" (
---     "emp_no" integer   NOT NULL,
---     "birth_date" varchar(30)   NOT NULL,
---     "first_name" varchar(30)   NOT NULL,
---     "last_name" varchar(30)   NOT NULL,
---     "gender" varchar(30)   NOT NULL,
---     "hire_date" varcahr(30)   NOT NULL,
---     CONSTRAINT "pk_employees" PRIMARY KEY (
---         "emp_no"
---      )
--- );
-
--- CREATE TABLE "salaries" (
---     "emp_no" integer   NOT NULL,
---     "salary" integer   NOT NULL,
---     "from_date" varchar(30)   NOT NULL,
---     "to_date" varchar(30)   NOT NULL
--- );
-
--- CREATE TABLE "titles" (
---     "emp_no" integer   NOT NULL,
---     "title" varchar(30)   NOT NULL,
---     "from_date" varchar(30)   NOT NULL,
---     "to_date" varchar(30)   NOT NULL
--- );
-
--- ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "employees" ("emp_no");
-
--- ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES "departments" ("dept_no");
-
--- ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES "departments" ("dept_no");
-
--- ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "employees" ("emp_no");
-
--- ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "employees" ("emp_no");
-
--- ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "employees" ("emp_no");
-
-
 --1
 select employees.emp_no, last_name, first_name, gender, salaries.salary 
 from employees
@@ -80,11 +8,11 @@ select emp_no, last_name, first_name, hire_date
 from employees
 where hire_date like '1986%';
 
---3 doesn't work
-select employees.emp_no, employees.last_name, employees.first_name, dept_manager.dept_no, departments.dept_name, dept_manager.from_date, dept_manager.to_date
-from employees
-inner join dept_manager on employees.emp_no = dept_manager.emp_no
-inner join departments on dept_mananger.dept_no = departments.dept_no;
+--3
+select dept_manager.emp_no, last_name, first_name, departments.dept_no, dept_name, to_date, from_date
+from dept_manager
+inner join employees on dept_manager.emp_no = employees.emp_no
+inner join departments on dept_manager.dept_no = departments.dept_no;
 
 --4
 select employees.emp_no, last_name, first_name, dept_name 
